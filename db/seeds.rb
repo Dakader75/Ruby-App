00753243736c88d554305e6511d40f3b044cd902
Alpine Devcamp <alpine@devcamp.com>
2017-06-22 16:46:36 +0000

    ver 1.1.7 configured nested attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,10 +46,11 @@ end
 end                
 puts "9 portfolio items created"   
 
-3.times do |technologies|
+3.times do |technology|
     Portfolio.last.technologies.create!(
-        name: "Technology #(technology)",
-        )
+        name: "Technology #(technology)"
+    )
 end
 
 puts "3 technologies created"
+
