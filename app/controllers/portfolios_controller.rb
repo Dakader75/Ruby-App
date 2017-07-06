6dc95c5269098cfa8fb8281dd1b266a3f74eae64
Alpine Devcamp <alpine@devcamp.com>
2017-07-06 17:56:03 +0000

    ver 1.4.1 bootstrap implemented

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+    before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
     layout 'portfolio'
     def index
         @portfolio_items = Portfolio.all
@@ -24,12 +25,9 @@ class PortfoliosController < ApplicationController
         end
     end
     def edit
-        @portfolio_item = Portfolio.find(params[:id])
     end
     
     def update
-    @portfolio_item = Portfolio.find(params[:id])
-    
         respond_to do |format|
             if @portfolio_item.update(portfolio_params)
                 format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
@@ -40,14 +38,12 @@ class PortfoliosController < ApplicationController
     end
     
     def show
-        @portfolio_item = Portfolio.find(params[:id])
     end
     
     def destroy
-        @portfolio_item = Portfolio.find(params[:id])
         
         @portfolio_item.destroy
-        
+
         respond_to do |format|
             format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
         end
@@ -61,5 +57,9 @@ class PortfoliosController < ApplicationController
                                          :body, 
                                          technologies_attributes: [:name]
                                          )
+    end
+    
+    def set_portfolio_item
+        @portfolio_item = Portfolio.find(params[:id])
     end
 end
